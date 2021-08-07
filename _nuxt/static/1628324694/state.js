a222482499b4cb39318037e6639374a6ea58e625
Jeremy Manoto <jeremy@manoto.com.au>
2021-08-07 18:24:55 +1000

    40dc27b

---
+++ _nuxt/static/1628324694/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1628324694",layout:"default",error:a,state:a,serverRendered:false,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b998c22b"}}}}(null,"\u002F"));
